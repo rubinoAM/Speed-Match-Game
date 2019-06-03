28ab4902f6da4155ea97e7e9d6111e0424208cf0
Michael Rubino <parkcathedralgrace@gmail.com>
2019-06-03 00:46:26 -0400

    Working on shape generation: All spaces generate shapes

--- public/scripts/createShapes.js
+++ public/scripts/createShapes.js
@@ -1,19 +1,16 @@
 function createShapes(){
-    const puzzle = parseDOM('puzzle','extension');
-    const heart = parseDOM('heart','favorite');
-    const note = parseDOM('note','audiotrack');
-    const moon = parseDOM('moon','brightness_2');
-    const sun = parseDOM('sun','brightness_7');
-    const flower = parseDOM('flower','local_florist');
+    const puzzle = htmlString('puzzle','extension');
+    const heart = htmlString('heart','favorite');
+    const note = htmlString('note','audiotrack');
+    const moon = htmlString('moon','brightness_2');
+    const sun = htmlString('sun','brightness_7');
+    const flower = htmlString('flower','local_florist');
 
     return [puzzle,heart,note,moon,sun,flower];
 }
 
-function parseDOM(shape,icon){
-    const parser = new DOMParser();
-    const string = `<div class="shape ${shape}">
-                        <i class="material-icons">${icon}</i>
-                    </div>`;
-    const htmlElem = parser.parseFromString(string, 'text/html');
-    return htmlElem.body.firstChild;
+function htmlString(shape,icon){
+    return `<div class="shape ${shape}">
+                <i class="material-icons">${icon}</i>
+            </div>`;
 }
